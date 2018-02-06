48b8247b2c6f2a19df28c7780e96a2b6ca717fcb
Clément Gérigné <clement.gerigne@gmail.com>
2018-02-06 13:01:09 +0100

    menu+console

--- src/main/fr/epsi/gravarmor/controller/MenuController.java
+++ src/main/fr/epsi/gravarmor/controller/MenuController.java
@@ -3,18 +3,12 @@ package main.fr.epsi.gravarmor.controller;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontSmoothingType;
 import javafx.scene.text.Text;
-
-import java.awt.*;
 
 import static javafx.scene.paint.Color.BLUE;
 import static javafx.scene.paint.Color.RED;
@@ -60,33 +54,38 @@ public class MenuController {
         Button boutonPasserLeTour = new Button("Passer le tour (F2)");
         boutonPasserLeTour.setPrefSize(140,50);
 
+        Button boutonDeplacer = new Button("Déplacement (F3)");
+        boutonDeplacer.setPrefSize(140,50);
 
-        RadioButton modeDeplacement = new RadioButton("Mode déplacement");
-        RadioButton modeTire = new RadioButton("Mode tire");
-
-        boutonPasserLeTour.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                consoleOutpout="Fin du tour";
-            }
-        });
-
-        boutonAjouterUnPion.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e) {
-                consoleOutpout="Pion ajouté";
-            }
-        });
+        Button boutonTirer = new Button("Tirer (F4)");
+        boutonTirer.setPrefSize(140,50);
 
         TextArea textConsole = new TextArea("\n"+consoleOutpout);
+
+        boutonPasserLeTour.setOnAction(e -> {
+            consoleOutpout +="Fin du tour\n\n";
+            textConsole.setText(consoleOutpout);
+            textConsole.setScrollTop(textConsole.getHeight());
+        });
+
+        boutonAjouterUnPion.setOnAction(e -> {
+            consoleOutpout +="Pion ajouté\n\n";
+            textConsole.setText(consoleOutpout);
+            textConsole.setScrollTop(textConsole.getHeight());
+
+        });
+
         textConsole.snappedBottomInset();
         textConsole.scrollTopProperty();
+        textConsole.setWrapText(false);
+        textConsole.setEditable(false);
 
         gridPane.add(textMainMenu,0,0);
         gridPane.add(boutonPasserLeTour,0,2);
         gridPane.add(boutonAjouterUnPion,0,3);
-        gridPane.add(textConsole,0,4);
-
+       // gridPane.add(boutonTirer,0,4);
+        //gridPane.add(boutonDeplacer,1,4);
+        gridPane.add(textConsole,0,5);
 
         gridPane.setVgap(20);
         gridPane.setHgap(20);
